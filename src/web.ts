65fcde40525c65e1f6547e035e89c3753ca51d2b
mustafaDab7ah <mdabah@snowheap.com>
2024-12-30 13:41:39 +0200

    add web functionlity

--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,12 @@ import { WebPlugin } from '@capacitor/core';
 import type { LoginWithUAEPassPlugin } from './definitions';
 
 export class LoginWithUAEPassWeb extends WebPlugin implements LoginWithUAEPassPlugin {
-  initialize(): Promise<void> {
-    throw new Error('Method not implemented.');
+  initialize(options: { clientId: string; clientSecret: string; environment: "production" | "qa" | "dev"; redirectUri: string; scope: string; state: string; successScheme: string; failScheme: string; signingScope: string; }): Promise<void> {
+    return new Promise((resolve) => {
+      console.log('Web initialize called with options:', options);
+      // Simulate initialization logic
+      resolve();
+    });
   }
   async login(): Promise<{ accessToken: string; userInfo: { uuid: string; firstName: string; lastName: string; email: string; } }> {
     console.log('Web login called');
